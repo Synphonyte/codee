e6d575a02fdb654a055deaadaf4a2ffd0354d366
maccesch <maccesch@synphonyte.com>
2025-11-19 23:50:20 +0100

    implemented Copy and Clone for CodecError

--- src/error.rs
+++ src/error.rs
@@ -7,3 +7,23 @@ pub enum CodecError<E, D> {
     #[error("failed to decode: {0}")]
     Decode(D),
 }
+
+impl<E, D> Clone for CodecError<E, D>
+where
+    E: Clone,
+    D: Clone,
+{
+    fn clone(&self) -> Self {
+        match self {
+            CodecError::Encode(e) => CodecError::Encode(e.clone()),
+            CodecError::Decode(d) => CodecError::Decode(d.clone()),
+        }
+    }
+}
+
+impl<E, D> Copy for CodecError<E, D>
+where
+    E: Copy,
+    D: Copy,
+{
+}
